9d50e3650f9501a5c689de176dc75c280a69ecfe
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-05-31 20:31:55 +0100

    Fetch and Display missions from API

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import '../styles/mission.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IconContext } from 'react-icons';
+import { FaTwitter, FaWikipediaW } from 'react-icons/fa';
+import { AiOutlineLink } from 'react-icons/ai';
+import { getMissions } from '../redux/missions/missionSlice';
 
-const Mission = () => (
-  <div>Mission</div>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  const { missions, isLoading } = useSelector((state) => state.mission);
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mission-container">
+      <IconContext.Provider value={{ color: '#34343a', className: 'icons', size: '18px' }}>
+        <table id="table-container">
+          <thead>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            {missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td className="mission-name">{mission.mission_name}</td>
+                <td className="mission-description">
+                  {mission.description}
+                  <br />
+                  <div className="logo-container">
+                    <a aria-label="website-button" href={mission.website}><AiOutlineLink /></a>
+                    <a aria-label="wiki-button" href={mission.wikipedia}><FaWikipediaW /></a>
+                    <a aria-label="twitter-button" href={mission.twitter}><FaTwitter /></a>
+                  </div>
+                </td>
+                <td className="table-btns">
+                  <button type="button" className="member-btn">Not A Member</button>
+                </td>
+                <td className="table-btns">
+                  <button type="button" className="join-btn">Join Mission</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </IconContext.Provider>
+    </div>
+  );
+};
 
 export default Mission;
--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -1,11 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+const initialState = {
+  missions: [],
+  isLoading: false,
+  error: null,
+};
+
+const url = 'https://api.spacexdata.com/v3/missions';
+
+export const getMissions = createAsyncThunk('missions/getMissions', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(url);
+    const missionArray = (response.data).map((mission) => mission);
+    return missionArray;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('Something Went Wrong');
+  }
+});
 
 const missionSlice = createSlice({
   name: 'mission',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMissions.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getMissions.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.missions = action.payload;
+      })
+      .addCase(getMissions.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
+export const { addMission } = missionSlice.actions;
 export default missionSlice.reducer;
