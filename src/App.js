ac2edfac587f42f2e857bb82e1d2d5309f1c163e
BranBayou <berhan.baye@gmail.com>
2023-06-01 16:46:54 +0300

    fix default home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Profile from './components/Profile';
 import Rockets from './components/Rockets';
@@ -10,9 +12,10 @@ function App() {
       <div className="App">
         <Navigation />
         <Routes>
+          <Route element={<Navigate to="/rockets" />} path="/" />
           <Route element={<Mission />} exact path="/missions" />
           <Route element={<Profile />} exact path="/my-profile" />
-          <Route element={<Rockets />} exact path="/" />
+          <Route element={<Rockets />} exact path="/rockets" />
         </Routes>
       </div>
     </BrowserRouter>
