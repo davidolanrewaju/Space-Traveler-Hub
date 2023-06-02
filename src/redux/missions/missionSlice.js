1a5708eb08f0005fcabafa0ee748e72e1cbac559
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-06-02 19:51:30 +0100

    Add mission test file and store button state when clicked

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -1,3 +1,5 @@
+//
+
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -5,6 +7,7 @@ const initialState = {
   missions: [],
   isLoading: false,
   error: null,
+  reservedMissions: [], // New state property to hold reserved mission IDs
 };
 
 const url = 'https://api.spacexdata.com/v3/missions';
@@ -12,7 +15,10 @@ const url = 'https://api.spacexdata.com/v3/missions';
 export const getMissions = createAsyncThunk('missions/getMissions', async (_, thunkAPI) => {
   try {
     const response = await axios.get(url);
-    const missionArray = (response.data).map((mission) => mission);
+    const missionArray = response.data.map((mission) => ({
+      ...mission,
+      reserved: false, // Add initial reserved property
+    }));
     return missionArray;
   } catch (error) {
     return thunkAPI.rejectWithValue('Something Went Wrong');
@@ -25,25 +31,19 @@ const missionSlice = createSlice({
   reducers: {
     joinMission: (state, action) => {
       const missionId = action.payload;
-      const newState = { ...state };
-      newState.missions = state.missions.map((mission) => {
-        if (mission.mission_id !== missionId) {
-          return mission;
-        }
-        return { ...mission, reserved: true };
-      });
-      return newState;
+      state.missions = state.missions.map((mission) => (
+        mission.mission_id !== missionId ? mission : { ...mission, reserved: true }
+      ));
+      state.reservedMissions.push(missionId);
+      sessionStorage.setItem('reservedMissions', JSON.stringify(state.reservedMissions));
     },
     leaveMission: (state, action) => {
       const missionId = action.payload;
-      const newState = { ...state };
-      newState.missions = state.missions.map((mission) => {
-        if (mission.mission_id !== missionId) {
-          return mission;
-        }
-        return { ...mission, reserved: false };
-      });
-      return newState;
+      state.missions = state.missions.map((mission) => (
+        mission.mission_id !== missionId ? mission : { ...mission, reserved: false }
+      ));
+      state.reservedMissions = state.reservedMissions.filter((id) => id !== missionId);
+      sessionStorage.setItem('reservedMissions', JSON.stringify(state.reservedMissions));
     },
   },
   extraReducers: (builder) => {
@@ -55,6 +55,17 @@ const missionSlice = createSlice({
       .addCase(getMissions.fulfilled, (state, action) => {
         state.isLoading = false;
         state.missions = action.payload;
+
+        // Retrieve reserved mission IDs from session storage
+        const storedMissions = JSON.parse(sessionStorage.getItem('reservedMissions'));
+        if (storedMissions) {
+          state.reservedMissions = storedMissions;
+          // Update the reserved property of missions based on stored mission IDs
+          state.missions = state.missions.map((mission) => ({
+            ...mission,
+            reserved: state.reservedMissions.includes(mission.mission_id),
+          }));
+        }
       })
       .addCase(getMissions.rejected, (state, action) => {
         state.isLoading = false;
